dc55d5d876dff0b533e0a2ca37837aab63c1cb2a
UPIMAN73 <joshcalzadi@gmail.com>
2018-01-13 16:28:54 -0500

    First Machine learning Project
    Just did a test run and it worked. We are currently doing Genetic Algorithm for the Strategy Planning. We are using the Jenetic Library for the algorithm.

---
+++ src/org/usfirst/frc/team2342/main/GMain.java
@@ -0,0 +1,35 @@
+package org.usfirst.frc.team2342.main;
+import io.jenetics.IntegerChromosome;
+import io.jenetics.IntegerGene;
+import io.jenetics.Genotype;
+import io.jenetics.engine.Engine;
+import io.jenetics.engine.EvolutionResult;
+import io.jenetics.util.Factory;
+ 
+public class GMain {
+ 
+	public static int eval(Genotype<IntegerGene> gt) {
+		return gt.getChromosome().as(IntegerChromosome.class).intValue();
+	}
+	
+    public static void main(String[] args) {
+        // 1.) Define the genotype (factory) suitable
+        //     for the problem.
+        Factory<Genotype<IntegerGene>> gtf =
+            Genotype.of(IntegerChromosome.of(0, 100));
+        System.out.println("Hello world!:\n" + gtf);
+ 
+        // 3.) Create the execution environment.
+        Engine<IntegerGene, Integer> engine = Engine
+            .builder(GMain::eval, gtf)
+            .build();
+ 
+        // 4.) Start the execution (evolution) and
+        //     collect the result.
+        Genotype<IntegerGene> result = engine.stream()
+            .limit(100)
+            .collect(EvolutionResult.toBestGenotype());
+ 
+        System.out.println("Hello World:\n" + result);
+    }
+}
