07d3085b9c73d03ee7040c38fc72c35a80b1d907
RaySL <sedna9801@gmail.com>
2018-01-20 14:57:04 -0500

    Super simple sim on 3 states working

--- src/org/usfirst/frc/team2342/main/GMain.java
+++ src/org/usfirst/frc/team2342/main/GMain.java
@@ -2,9 +2,12 @@ package org.usfirst.frc.team2342.main;
 import io.jenetics.IntegerChromosome;
 import io.jenetics.IntegerGene;
 import io.jenetics.Chromosome;
+import io.jenetics.DoubleChromosome;
+import io.jenetics.DoubleGene;
 import io.jenetics.Genotype;
 import io.jenetics.engine.Engine;
 import io.jenetics.engine.EvolutionResult;
+import io.jenetics.util.DoubleRange;
 import io.jenetics.util.Factory;
  
 public class GMain {
@@ -12,21 +15,27 @@ public class GMain {
     public static void main(String[] args) {
         // 1.) Define the genotype (factory) suitable
         //     for the problem.
-        Factory<Genotype<IntegerGene>> gtf =
-            Genotype.of(IntegerChromosome.of(0, 1));
-        System.out.println("Hello world!:\n" + gtf);
+        Factory<Genotype<DoubleGene>> gtf =
+            Genotype.of(
+            		DoubleChromosome.of(
+            				DoubleRange.of(0, 1),
+            				3
+            		),
+            		3
+            );
+        System.out.println("Initial:\n" + gtf);
  
         // 3.) Create the execution environment.
-        Engine<IntegerGene, Integer> engine = Engine
+        Engine<DoubleGene, Integer> engine = Engine
             .builder(Simulation::eval, gtf)
             .build();
  
         // 4.) Start the execution (evolution) and
         //     collect the result.
-        Genotype<IntegerGene> result = engine.stream()
+        Genotype<DoubleGene> result = engine.stream()
             .limit(100)
             .collect(EvolutionResult.toBestGenotype());
  
-        System.out.println("Hello World:\n" + result);
+        System.out.println("Final:\n" + result);
     }
 }
--- src/org/usfirst/frc/team2342/main/Simulation.java
+++ src/org/usfirst/frc/team2342/main/Simulation.java
@@ -1,14 +1,19 @@
 package org.usfirst.frc.team2342.main;
 
 import io.jenetics.Chromosome;
+import io.jenetics.DoubleGene;
+import io.jenetics.Gene;
 import io.jenetics.Genotype;
 import io.jenetics.IntegerChromosome;
 import io.jenetics.IntegerGene;
+import io.jenetics.NumericGene;
+import jeigen.DenseMatrix;
+
+import static jeigen.Shortcuts.*;
 
 public class Simulation {
-	int state;
 	
-	public static int[][] genotypeToMatrix(Genotype<IntegerGene> gtf) {
+	/*public static int[][] genotypeToMatrix(Genotype<IntegerGene> gtf) {
 		int[][] matrix = new int[gtf.length()][gtf.getChromosome(0).length()];
 		int x = 0;
 		int y = 0;
@@ -21,10 +26,43 @@ public class Simulation {
 			y++;
 		}
 		return matrix;
+	}*/
+	
+	public static <N extends Number & Comparable<? super N>,G extends NumericGene<N,G>> DenseMatrix genotypeToMatrix(Genotype<G> gt) {
+		int rows = gt.length();
+		int cols = gt.getChromosome().length();
+		
+		DenseMatrix mat = zeros(rows, cols);
+		
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				mat.set(i, j, (double) gt.get(i, j).getAllele());
+			}
+		}
+		
+		return mat;
 	}
 	
-	public static int eval(Genotype<IntegerGene> gt) {
-		int[][] matrix = genotypeToMatrix(gt);
-		return 0;
+	public static int eval(Genotype<DoubleGene> gt) {
+		DenseMatrix matrix = genotypeToMatrix(gt);
+		System.out.println(matrix);
+		
+		DenseMatrix state = zeros(matrix.cols, 1);
+		state.set(0, 0, 1);
+		System.out.println(state);
+		
+		// Starts in State A:
+		
+		// [ A ]    [ 1 ]
+		// [ B ] -> [ 0 ]
+		// [ C ]    [ 0 ]
+		
+		for (int i = 0; i < 2; i++) {
+			state = matrix.mmul(state);
+		}
+		
+		
+		// Try to end on State C
+		return (int) state.get(2, 0) * 1000;
 	}
 }
